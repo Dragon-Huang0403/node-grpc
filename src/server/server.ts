9db60eaa9a54b88d00bc096f97aea8ac2e188761
Dragon Huang <j0918023423@gmail.com>
2023-12-26 11:21:45 +0800

    feat(server): handle add one

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,29 @@
-var counterMsg = require("../proto/counter_pb.js");
+import { AddOneRequest, AddOneResponse } from "../proto/counter_pb";
+import { CounterService } from "../proto/counter_grpc_pb";
 
-console.log("Hello World!");
-console.log(counterMsg);
+import * as grpc from "@grpc/grpc-js";
+
+const addOne = (
+  call: grpc.ServerUnaryCall<AddOneRequest, AddOneResponse>,
+  callback: grpc.sendUnaryData<AddOneResponse>
+) => {
+  const result = call.request.getValue() + 1;
+  const response = new AddOneResponse();
+  response.setValue(result);
+
+  callback(null, response);
+};
+
+function main() {
+  var server = new grpc.Server();
+  server.addService(CounterService, { addOne });
+  server.bindAsync(
+    "0.0.0.0:50051",
+    grpc.ServerCredentials.createInsecure(),
+    () => {
+      server.start();
+    }
+  );
+}
+
+main();
